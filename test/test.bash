749744babb59ab7c302095a6a2f27aa6d0576a3e
23C1130 <s23c1130sm@s.chibakoudai.jp>
2024-11-28 13:14:28 +0900

    [Fix] Github Actions の修正8

--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,6 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 11 ros2 launch mypkg talk_listen.launch.py > $dir/ros2_ws/mypkg.log
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat $dir/ros2_ws/mypkg.log |
-grep 'Listen: 10'
+cat /tmp/mypkg.log | grep 'Listen: 10'
