5b2f26b8f89dd7e4400551cd135b7708cfcbee6b
23C1130 <s23c1130sm@s.chibakoudai.jp>
2024-11-28 14:44:08 +0900

    Test

--- test/test.bash
+++ test/test.bash
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 dir=~
-[ "$1" != "" ] && dir="$1"
+[ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-source $dir/ros2_ws/install/setup.bash
-source $dir/ros2_ws/install/local_setup.bash
-
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log | grep 'Listen: 10'
+cat /tmp/mypkg.log |
+grep 'Listen: 10'
