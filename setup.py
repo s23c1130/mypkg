60f99b514a41b7a84649b420c8a8293a2bc8eb16
23C1130 <s23c1130sm@s.chibakoudai.jp>
2024-11-28 15:08:48 +0900

    fix 22

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'mypkg'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='23C1130',
-    maintainer_email='s23c1130@s.chibaoudai,jp',
-    description='TODO: Robosys',
-    license='TODO: BSD-3-Clause',
+    maintainer='Toki Makabe',
+    maintainer_email='s23c1130sm@s.chibakoudai.jp',
+    description='mypkg',
+    license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
