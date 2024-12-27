14546bbc5eb6442dbe23fe1323d851d35586b12b
23C1130 <s23c1130sm@s.chibakoudai.jp>
2024-12-27 13:14:34 +0900

    FIx2

--- test/test.bash
+++ test/test.bash
@@ -1,13 +1,19 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
+sudo apt -y update
 sudo apt -y install xdotool
+sudo apt -y install xvfb
+
+Xvfb -ac :99 -screen 1 1280x1024x24
+export DISPLAY=:99
+
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 3 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 x=`xdotool getmouselocation | awk '{print substr($1, 3)}'`
 y=`xdotool getmouselocation | awk '{print substr($2, 3)}'`
